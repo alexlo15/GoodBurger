042945972643124f863b1b4abf5932bfa90c1ac4
Alex Lo <loalexander389@gmail.com>
2019-08-19 18:11:14 -0400

    working

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-### Schema
+
 
 CREATE DATABASE burgers_db;
 USE burgers_db;
@@ -7,6 +7,6 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
-	mondo BOOLEAN DEFAULT false,
+	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 
-INSERT INTO burgers (burger_name, mondo) VALUES ('Mondo Burger', true);
-INSERT INTO burgers (burger_name, mondo) VALUES ('Good Burger', false);
-INSERT INTO burgers (burger_name, mondo) VALUES ('Krugs Newark', false);
-INSERT INTO burgers (burger_name, mondo) VALUES ('Krabby Patty', false);
-INSERT INTO burgers (burger_name, mondo) VALUES ('Chum Bucket Special', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mondo Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krugs Newark', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krabby Patty', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chum Bucket Special', true);
 
