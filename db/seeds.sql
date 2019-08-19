14ed9bfb1ba891604ce5292ede180410da9f96fb
Alex Lo <loalexander389@gmail.com>
2019-08-19 16:38:35 -0400

    mvc

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger_name, mondo) VALUES ('Mondo Burger', true);
+INSERT INTO burgers (burger_name, mondo) VALUES ('Good Burger', false);
+INSERT INTO burgers (burger_name, mondo) VALUES ('Krugs Newark', false);
+INSERT INTO burgers (burger_name, mondo) VALUES ('Krabby Patty', false);
+INSERT INTO burgers (burger_name, mondo) VALUES ('Chum Bucket Special', true);
+
